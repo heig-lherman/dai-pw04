32c2e98a41aa59f781ede1436f22b884bcfbada7
Loïc Herman <loic@loicherman.ch>
2024-01-09 17:06:39 +0100

    feat: terraform configuration for DNS records
    closes #3

---
+++ terraform/records.tf
@@ -0,0 +1,15 @@
+resource "cloudflare_record" "whoami" {
+  zone_id = data.cloudflare_zone.main.id
+  type    = "A"
+  name    = "whoami.dai.heig"
+  value   = "185.144.38.63"
+  proxied = false
+}
+
+resource "cloudflare_record" "api" {
+  zone_id = data.cloudflare_zone.main.id
+  type    = "A"
+  name    = "api.dai.heig"
+  value   = "185.144.38.63"
+  proxied = false
+}
